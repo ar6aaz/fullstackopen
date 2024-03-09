643c88368320a01e7c151f1278b015d489f8423a
ar6aaz <114828311+ar6aaz@users.noreply.github.com>
2024-03-09 10:49:42 +0000

    completed exercise 2.18

--- part2/restcountries/src/components/Country.jsx
+++ part2/restcountries/src/components/Country.jsx
@@ -1,5 +1,27 @@
 import { useState } from "react";
 
+const SingleCountry = ({filteredCountries}) => {
+  return (
+    <>
+      {filteredCountries
+      .map(country => (
+        <>
+        <h1 key={country.id}>{country.name.common}</h1>
+        <div key={country.id}>capital: {country.capital}</div>
+        <div key={country.id}>area: {country.area}</div>
+        <h4>languages: </h4>
+        {Object.entries(country.languages).map(([code, language], index) => (
+          <div key={index}>
+            <ul><li>{language}</li></ul>
+          </div>
+        ))}
+        <div>{country.flag}</div>
+        </>
+      ))}
+    </>
+  )
+}
+
 const ShowAllCountries = ({countries}) => {
   return (
     <>
@@ -24,24 +46,23 @@ const ShowFilteredCountries = ({countries, countryName}) => {
   const filteredCountries = countries != null
         ? countries
             .filter(country => country.name.common.toLowerCase().includes(countryName.toLowerCase()))
-            .map(country => country.name.common)
         : [];
-
-  console.log("filteredCountries: ", filteredCountries)
 
   return (
     <>
       {
-       filteredCountries.length > 10
-       ?
-          <p>Too many matches, specify another filter</p>
-       : 
-        filteredCountries
-          .map(country => (
-            <div key={country.id}>
-              {country}
-            </div>
-          ))
+        (filteredCountries.length > 10 && <p>Too many matches, specify another filter</p>)
+        ||
+        (filteredCountries.length === 1 && <SingleCountry filteredCountries={filteredCountries} />)
+        ||
+        (
+         filteredCountries
+           .map(country => (
+             <div key={country.id}>
+               {country.name.common}
+             </div>
+           ))
+        )
       }
     </>
   );
