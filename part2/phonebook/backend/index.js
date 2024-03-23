db544533fe30d22875667dc3f0c947139f7a7920
ar6aaz <114828311+ar6aaz@users.noreply.github.com>
2024-03-23 11:03:25 +0000

    make application ready to deploy to render

--- part2/phonebook/backend/index.js
+++ part2/phonebook/backend/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 var morgan = require('morgan')
-
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
+app.use(express.static('dist'))
+
 // app.use(morgan('tiny'))
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
@@ -86,6 +90,6 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
