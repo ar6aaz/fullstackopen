9f901ce5e9223c070af15a7ee3404e87eea727b6
ar6aaz <114828311+ar6aaz@users.noreply.github.com>
2024-03-23 08:39:29 +0000

    moved notes_backend to part3

--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://ubiquitous-orbit-pjwgvq66q62rwvx.app.github.dev:3001/notes'
+const baseUrl = 'https://scaling-couscous-g44qggpj9w6cvwj5-3001.app.github.dev/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-
 app.use(express.json())
+app.use(cors())
 
 let notes = [
     {
@@ -70,6 +71,6 @@ let notes = [
     response.json(note)
   })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
