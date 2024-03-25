d04cb96afca73c8fea834cb96f9bbf018d389df7
ar6aaz <114828311+ar6aaz@users.noreply.github.com>
2024-03-25 12:38:20 +0000

    completed exeercise 3.14

--- part2/phonebook/backend/index.js
+++ part2/phonebook/backend/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 var morgan = require('morgan')
 const cors = require('cors')
+const Phonebook = require('./models/phonebook')
 const app = express()
 
 app.use(cors())
@@ -12,42 +14,42 @@ app.use(morgan(':method :url :status :response-time ms - :res[content-length] :b
 
 app.use(express.json())
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons',(request, response) => {
-    response.json(persons)
+  Phonebook.find({}).then(notes => {
+    response.json(notes)
+})
 })
 
 app.get('/api/persons/:id',(request, response) => {
-  const id = Number(request.params.id)
-  const personData = persons.find(person => person.id === id)
-  personData ?
-  response.json(personData)
-  : response.status(404).end()
+  Phonebook.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -66,6 +68,7 @@ app.get('/info', (request, response) => {
 const generateId = () => {
   return Math.floor(Math.random() * Date.now())
 }
+
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
@@ -75,21 +78,24 @@ app.post('/api/persons', (request, response) => {
       })
   }
 
-  if(persons.find(person => person.name === body.name)){
-    return response.status(400).json({ 
-      error: 'name must be unique' 
-    })
-  }
+  // if(persons.find(person => person.name === body.name)){
+  //   return response.status(400).json({ 
+  //     error: 'name must be unique' 
+  //   })
+  // }
+  const phonebook = new Phonebook({
+    name: body.name,
+    number: body.number,
+  })
 
-  const person = {
-      id: generateId(),
-      name: body.name,
-      number: body.number,
-  }
-  persons = persons.concat(person)
-  response.json(person)
+  phonebook.save().then(savedPhonebook => {
+    response.json(savedPhonebook)
+  })
+
+  // persons = persons.concat(person)
+  // response.json(person)
 })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+console.log('Server running on port 3001')
