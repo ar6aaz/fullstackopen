b05407dbbceb1a46b38e879bb9b32f2037c86f98
ar6aaz <114828311+ar6aaz@users.noreply.github.com>
2024-03-25 13:13:08 +0000

    added error handling

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -2,11 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const Note = require('./models/note')
-
 const app = express()
+
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
+app.use(requestLogger)
 
   app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -18,10 +19,16 @@ app.use(express.static('dist'))
     })
   })
 
-  app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-      response.json(note)
+  app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+      .then(note => {
+        if (note) {
+          response.json(note)
+        } else {
+          response.status(404).end()
+        }
     })
+    .catch(error => next(error))
   })
 
   app.delete('/api/notes/:id', (request, response) => {
@@ -54,6 +61,21 @@ app.use(express.static('dist'))
       response.json(savedNote)
     })
   })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+} 
+  // handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+  
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
