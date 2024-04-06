bed07033bb0b63bd20c090f39dad5352dc801c1a
ar6aaz <114828311+ar6aaz@users.noreply.github.com>
2024-04-06 11:31:47 +0000

    completed exercise 4.7

--- part4/blog/tests/blog.test.js
+++ part4/blog/tests/blog.test.js
@@ -26,15 +26,23 @@ describe('total likes', () => {
         {
           _id: '5a422aa71b54a676234a17f8',
           title: 'Go To Statement Considered Not Harmful',
-          author: 'Edsger W. Dijkstra',
+          author: 'Arbaaz',
           url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra67.pdf',
           likes: 4,
           __v: 0
         },
         {
+          _id: '5a422aa712fa676234a17f8',
+          title: 'Copy of blog',
+          author: 'Arbaaz',
+          url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra27.pdf',
+          likes: 1,
+          __v: 0
+        },
+        {
             _id: '5a422aa71b54a6s6234d17f8',
             title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
+            author: 'Edsger W. Dijkstrsa',
             url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
             likes: 3,
             __v: 0
@@ -42,7 +50,7 @@ describe('total likes', () => {
         {
             _id: '5a422aa71b54a676f24d17f8',
             title: 'Go To Statement Considered Average',
-            author: 'Edsger W. Dijkstra',
+            author: 'Arbaaz',
             url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra69.pdf',
             likes: 5,
             __v: 0
@@ -60,10 +68,18 @@ describe('total likes', () => {
 
     test('of of a bigger list is calculated right', () => {
         const result = listHelper.totalLikes(blogs)
-        assert.strictEqual(result, 12)
+        assert.strictEqual(result, 13)
     })
 
     test('being max is returned as fav blog', () => {
-        assert.strictEqual(listHelper.favouriteBlog(blogs), 2)
+        assert.deepStrictEqual(listHelper.favouriteBlog(blogs), blogs[3])
     })
+
+    test('being max, author is returned', () => {
+      assert.deepStrictEqual(listHelper.mostBlogs(blogs), { author: 'Arbaaz', blogs: 3 })
+  })
+
+  test('being max, author with all likes summed is returned', () => {
+    assert.deepStrictEqual(listHelper.mostLikes(blogs), { author: 'Arbaaz', likes: 10 })
+})
   })
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -9,17 +9,59 @@ const totalLikes = (blogs) => {
 }
 
 const favouriteBlog = (blogs) => {
-    // const maxValue = Math.max(blogs)
-    // const maxIndex = blogs.indexOf(maxValue)
-    // console.log("maxIndex: ", maxIndex)
     const maxIndex = blogs.reduce((maxIndex, blog, currentIndex, arr) => {
         return blog.likes > arr[maxIndex].likes ? currentIndex : maxIndex;
     }, 0);
-    return maxIndex;
+    return blogs[maxIndex];
+}
+
+const mostBlogs = (blogs) => {
+    const authorCounts = {};
+    blogs.forEach(blog => {
+        const author = blog.author;
+        authorCounts[author] = (authorCounts[author] || 0) + 1;
+    });
+
+    let topAuthor;
+    let maxBlogs = 0;
+    for (const author in authorCounts) {
+        if (authorCounts[author] > maxBlogs) {
+            topAuthor = author;
+            maxBlogs = authorCounts[author];
+        }
+    }
+    return {
+        author: topAuthor,
+        blogs: maxBlogs
+    };
+}
+
+const mostLikes = (blogs) => {
+    const authorLikes = {};
+    blogs.forEach(blog => {
+        const author = blog.author;
+        authorLikes[author] = (authorLikes[author] || 0) + blog.likes;
+    });
+    
+    let topAuthor;
+    let maxLikes = 0;
+    for (const author in authorLikes) {
+        if (authorLikes[author] > maxLikes) {
+            topAuthor = author;
+            maxLikes = authorLikes[author];
+        }
+    }
+
+    return {
+        author: topAuthor,
+        likes: maxLikes
+    };
 }
 
 module.exports = {
     dummy,
     totalLikes,
-    favouriteBlog
+    favouriteBlog,
+    mostBlogs,
+    mostLikes
 }
