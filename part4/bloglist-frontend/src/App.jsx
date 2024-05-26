ad4f56297f3f8720628ebf038cf443b8b54b2352
ar6aaz <114828311+ar6aaz@users.noreply.github.com>
2024-05-26 18:50:22 +0000

    implemented auth"

--- part4/bloglist-frontend/src/App.jsx
+++ part4/bloglist-frontend/src/App.jsx
@@ -1,12 +1,13 @@
+import React from 'react';
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
-const [username, setUsername] = useState('') 
-const [password, setPassword] = useState('')
-const [user, setUser] = useState(null)
 
 const App = () => {
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
   const [blogs, setBlogs] = useState([])
 
   useEffect(() => {
